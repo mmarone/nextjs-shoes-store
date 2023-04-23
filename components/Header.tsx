9a38e7b4e013a62f1b0ef8d383b8786c471b6fef
Nataphong Somjai <nataphong@sellsuki.com>
2023-04-23 20:25:26 +0700

    add product detail page

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,13 +26,11 @@ const Header = () => {
     } else {
       setShow('translate-y-0')
     }
-
     setLastScrollY(window.scrollY)
   }
 
   useEffect(() => {
     window.addEventListener('scroll', controlNavbar)
-
     return () => {
       window.removeEventListener('scroll', controlNavbar)
     }
@@ -48,7 +46,7 @@ const Header = () => {
 
   return (
     <div
-      className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 translate-y-0 ${show}`}
+      className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
     >
       <Wrapper className="h-[60px] flex justify-between items-center">
         <Link href="/">
